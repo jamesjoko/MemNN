40b797f9ff4d5373f449bf33fa1794ad48136dda
James Joko <45160871+jamesjoko@users.noreply.github.com>
2021-10-25 14:22:02 -0400

    small edit to comment in read_data.py

--- read_data.py
+++ read_data.py
@@ -1,15 +1,18 @@
+#reference: https://gist.github.com/danoneata/49a807f47656fedbb389
 import numpy as np
 
+#read a .ivecs file into numpy array
 def ivecs_read(fname):
     a = np.fromfile(fname, dtype='int32')
     d = a[0]
     return a.reshape(-1, d + 1)[:, 1:].copy()
 
-
+#read a .fvecs file into numpy array
 def fvecs_read(fname):
     return ivecs_read(fname).view('float32')
 
 
+#print vector space of all files in the siftsmall dataset
 base = fvecs_read("siftsmall/siftsmall_base.fvecs")
 print(base)
 print(base.shape)
@@ -25,4 +28,3 @@ print(learn.shape)
 query = fvecs_read("siftsmall/siftsmall_query.fvecs")
 print(query)
 print(query.shape)
-#//https://gist.github.com/danoneata/49a807f47656fedbb389
